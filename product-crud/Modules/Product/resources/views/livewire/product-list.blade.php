52023edb1b273080e6e030e0118361415dd4d7bb
Muhammad Emad Hussein <127058766+MuhamedEmadHussein@users.noreply.github.com>
2025-07-01 04:04:39 +0300

    Update product-list.blade.php
    Change to Product-list view

--- product-crud/Modules/Product/resources/views/livewire/product-list.blade.php
+++ product-crud/Modules/Product/resources/views/livewire/product-list.blade.php
@@ -91,7 +91,6 @@
 
     <!-- Product List -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        @dd($products)
         @forelse($products as $product)
             
             <div class="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition">
