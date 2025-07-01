9c7d81498ce795f95db2c76422b681b03816b4fe
MuhamedEmadHussein <mohamedemadhu01019@gmail.com>
2025-07-01 03:57:24 +0200

    Sure! Pl

--- product-crud/Modules/Product/resources/views/livewire/product-list.blade.php
+++ product-crud/Modules/Product/resources/views/livewire/product-list.blade.php
@@ -91,9 +91,7 @@
 
     <!-- Product List -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        @dd($products)
         @forelse($products as $product)
-            
             <div class="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition">
                 <img src="{{ $product['image'] }}" alt="{{ $product['name'] }}" 
                      class="w-full h-48 object-cover rounded-md mb-4"
